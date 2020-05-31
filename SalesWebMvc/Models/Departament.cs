8136749f20ae109f1e5912ae7af80329ddbcb00b
Adenilton Pereira <adenilton_23@yahoo.com.br>
2020-05-31 13:07:04 -0300

    Other entities and second migration

--- SalesWebMvc/Models/Departament.cs
+++ SalesWebMvc/Models/Departament.cs
@@ -1,15 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-
 namespace SalesWebMvc.Models
+
 {
     public class Departament
     {
         public int Id { get; set; }
-        public string Name { get; set; }    
+        public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
 
+        public Departament()
+        {
 
+        }
+
+        public Departament(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        // Adiciona Funcionario Tabela Departamento
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            // Somar total de vendas na Tabela de vendedores por periodo
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
     }
 }
