c272514aee62a487d0f5f7333fc8a91e8f9657f1
Adenilton Pereira <adenilton_23@yahoo.com.br>
2020-06-05 23:59:25 -0300

    SellerService and basic FindAll

---
+++ SalesWebMvc/Service/SellerService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Service
+{
+    public class SellerService
+    {
+        private readonly SalesWebMvcContext _context;
+
+        // Construtor injecao de dependencia
+        public SellerService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+        // Retorna lista todos vendedores
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+
+
+
+    }
+}
