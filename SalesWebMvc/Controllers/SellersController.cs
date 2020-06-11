0a8ef246724d9ad64f36e6df9e0d118f11047e8c
Adenilton Pereira <adenilton_23@yahoo.com.br>
2020-06-11 02:17:07 -0300

    Seller details and eager loading

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -68,13 +68,24 @@ namespace SalesWebMvc.Controllers
         {
             _sellerService.Remove(id);
             return RedirectToAction(nameof(Index));
+        }
 
+        public IActionResult Details(int? id)
+        {
+            // Verificar id nulo
+            if (id == null)
+            {
+                return NotFound();
+            }
+            // Pegar obj no banco
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
 
         }
-        
-
-
-
 
     }
 }
